203766530e54bf658e42e00e34aef443ec4d9bae
Felix Müllner <felix.muellner.96@gmail.com>
2019-06-04 00:21:58 +0200

    Synchronize guild icons with YouTube profile picture

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ require('dotenv').config();
 require('./configureLogger.js');
 
 const discord = require('discord.js');
+const got = require('got');
 const logger = require('winston').loggers.get('default');
+
+const crypto = require('crypto');
+const fs = require('fs');
+const path = require('path');
+
+const youtube = require('./youtube.js');
 
 const client = new discord.Client();
 
 client.once('ready', () => {
-	// TODO start YT polling interval
+	setInterval(checkYouTubeAvatar, process.env.YOUTUBE_POLLING_INTERVAL * 1000);
 });
 
 client.on('ready', () => {
@@ -19,3 +26,69 @@ client.login(process.env.BOT_TOKEN).catch(err => {
 	logger.fatal(err);
 	process.exit(1);
 });
+
+async function checkYouTubeAvatar() {
+	try {
+		logger.debug('Checking YouTube channel');
+
+		const channelRes = await youtube.channels.list({
+			id: process.env.YOUTUBE_CHANNEL_ID,
+			part: 'id,snippet',
+		});
+
+		const channelObj = channelRes.data.items[0];
+		const channelName = channelObj.snippet.title;
+
+		const avatarRes = await got(channelObj.snippet.thumbnails.high.url, {
+			encoding: null,
+		});
+		const avatarBuffer = avatarRes.body;
+
+		logger.debug('Start hashing');
+		const avatarHash = crypto.createHash('sha1').update(avatarBuffer).digest('hex');
+		logger.debug(`sha1: ${avatarHash}`);
+
+		// compare to saved hash
+		let lastAvatarHash;
+		try {
+			lastAvatarHash = fs.readFileSync(path.join(__dirname, 'lastAvatarHash.txt'), 'utf8');
+		} catch (err) {
+			// it's not a problem if the file doesn't exist
+			if (err.code !== 'ENOENT') {
+				throw err;
+			}
+		}
+		if (avatarHash === lastAvatarHash) {
+			logger.debug(`Avatar not changed for YouTube channel "${channelName}"`);
+			return;
+		}
+		fs.writeFileSync(path.join(__dirname, 'lastAvatarHash.txt'), avatarHash);
+		logger.info(`Changed avatar detected for YouTube channel "${channelName}"`);
+
+		// update guild icons
+		const guildIds = process.env.DISCORD_GUILDS.split(',');
+		for (const guildId of guildIds) {
+			const guild = client.guilds.get(guildId);
+			if (!guild) {
+				logger.warn(`Bot is not in guild ${guildId}, skipping`);
+				continue;
+			}
+			try {
+				await guild.setIcon(avatarBuffer, `Syncing guild icon with YouTube Channel "${channelName}"`);
+				logger.verbose(`Changed guild icon for "${guild.name}" (${guild.id})`);
+			} catch (err) {
+				if (err.message === 'Missing Permissions') {
+					logger.error(`Missing permissions in guild "${guild.name}" (${guild.id}), skipping`);
+				} else {
+					throw err;
+				}
+			}
+		}
+
+		// update bot avatar
+		await client.user.setAvatar(avatarBuffer);
+	} catch (err) {
+		logger.error('Error while checking YouTube channel:');
+		logger.error(err);
+	}
+}
